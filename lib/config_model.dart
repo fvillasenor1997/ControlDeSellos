c0a630bb76fb02ef6676f2fd643a198182e84462
fvillasenor1997 <130634903+fvillasenor1997@users.noreply.github.com>
2025-09-25 03:52:48 -0700

    Update config_model.dart

--- lib/config_model.dart
+++ lib/config_model.dart
@@ -1,6 +1,11 @@
 class ConfigModel {
   List<Map<String, dynamic>> departments;
   Map<String, double> rowHeights;
+  double tableBottomOffset; // Nueva propiedad
 
-  ConfigModel({required this.departments, required this.rowHeights});
+  ConfigModel({
+    required this.departments, 
+    required this.rowHeights,
+    required this.tableBottomOffset, // Nueva propiedad
+  });
 }
