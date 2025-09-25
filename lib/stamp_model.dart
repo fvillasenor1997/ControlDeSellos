ad9a090ba2e10de414b51e8c9e2a07148f8aeedf
fvillasenor1997 <130634903+fvillasenor1997@users.noreply.github.com>
2025-09-24 20:31:15 -0700

    Create stamp_model.dart

---
+++ lib/stamp_model.dart
@@ -0,0 +1,24 @@
+import 'package.flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'stamp_model.g.dart';
+
+@JsonSerializable()
+class Stamp {
+  String text;
+  int colorValue;
+  double fontSize;
+  bool isBold;
+  String shape; // 'rectangle' or 'circle'
+
+  Stamp({
+    required this.text,
+    this.colorValue = 0xFF000000, // Negro por defecto
+    this.fontSize = 24.0,
+    this.isBold = true,
+    this.shape = 'rectangle',
+  });
+
+  factory Stamp.fromJson(Map<String, dynamic> json) => _$StampFromJson(json);
+  Map<String, dynamic> toJson() => _$StampToJson(this);
+}
