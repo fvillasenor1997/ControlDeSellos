e935f6f7ef12036055ac4de42a932b128400a927
fvillasenor1997 <130634903+fvillasenor1997@users.noreply.github.com>
2025-09-25 18:05:46 -0700

    Update config_model.dart

--- lib/config_model.dart
+++ lib/config_model.dart
@@ -1,6 +1,7 @@
 class ConfigModel {
   List<Map<String, dynamic>> departments;
   Map<String, double> rowHeights;
+  Map<String, String> fixedTexts;
 
-  ConfigModel({required this.departments, required this.rowHeights});
+  ConfigModel({required this.departments, required this.rowHeights, required this.fixedTexts});
 }
