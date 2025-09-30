06f4d4118478421790c206e2f115188c7e3fafa7
fvillasenor1997 <130634903+fvillasenor1997@users.noreply.github.com>
2025-09-29 22:09:18 -0700

    Update config_model.dart

--- lib/config_model.dart
+++ lib/config_model.dart
@@ -3,22 +3,30 @@ import 'dart:convert';
 class ConfigModel {
   List<Map<String, dynamic>> departments;
   Map<String, double> rowHeights;
+  double copiedHeaderHeight; // Nueva propiedad
 
-  ConfigModel({required this.departments, required this.rowHeights});
+  ConfigModel({
+    required this.departments,
+    required this.rowHeights,
+    this.copiedHeaderHeight = 30.0, // Valor por defecto
+  });
 
-  // Nuevo: convierte el modelo a un mapa para guardarlo en JSON
+  // Convierte el modelo a un mapa para guardarlo en JSON
   Map<String, dynamic> toJson() {
     return {
       'departments': departments,
       'rowHeights': rowHeights,
+      'copiedHeaderHeight': copiedHeaderHeight, // Añadir a JSON
     };
   }
 
-  // Nuevo: crea un modelo a partir de un mapa (cargado desde JSON)
+  // Crea un modelo a partir de un mapa (cargado desde JSON)
   factory ConfigModel.fromJson(Map<String, dynamic> json) {
     return ConfigModel(
       departments: List<Map<String, dynamic>>.from(json['departments']),
       rowHeights: Map<String, double>.from(json['rowHeights']),
+      // Cargar desde JSON con un valor por defecto si no existe
+      copiedHeaderHeight: (json['copiedHeaderHeight'] as num?)?.toDouble() ?? 30.0,
     );
   }
 }
